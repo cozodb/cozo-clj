c228db2dded465ac3deb4aff82b49608aed7dc7b
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-09 20:25:40 +0800

    bump version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject com.github.zh217/cozo-clj "0.1.0"
+(defproject com.github.zh217/cozo-clj "0.1.6"
   :description "CozoDb Clojure bindings"
   :url "http://github.com/cozodb/cozo"
   :signing {:gpg-key "hu.ziyang@cantab.net"}
   :license {:name "MIT/Apache-2.0/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "2.4.0"]
-                 [com.github.zh217/cozo-lib-java "0.1.0"]]
+                 [com.github.zh217/cozo-lib-java "0.1.6"]]
   :repl-options {:init-ns cozo-clj.core})
